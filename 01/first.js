242d1daed9d2d96f2169b18e77b716ca6335a1a1
Albin Hallden <albinhallden@gmail.com>
2016-12-01 17:25:38 +0100

    Add solution for day 01 - first task

---
+++ 01/first.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const dirs = ['N', 'E', 'S', 'W'];
+
+let direction = 0; // 0 = N, 1 = E, 2 = S, 3 = W
+let stepsX = 0;
+let stepsY = 0;
+
+fs.readFileSync(path.join('input.txt')).toString()
+  .split(',')
+  .map(item => item.trim())
+  .forEach(item => {
+    if (item[0] === 'R') direction++;
+    else if (item[0] === 'L') direction--;
+    direction = direction < 0 ? 3 : direction;
+    
+    const length = parseInt(item.substring(1));
+
+    if (direction % 4 === 0) stepsY += length;
+    else if (direction % 4 === 1) stepsX += length;
+    else if (direction % 4 === 2) stepsY -= length;
+    else if (direction % 4 === 3) stepsX -= length;
+  });
+
+console.log((stepsY < 0 ? stepsY *= -1 : stepsY) + (stepsX < 0 ? stepsX *= -1 : stepsX));
