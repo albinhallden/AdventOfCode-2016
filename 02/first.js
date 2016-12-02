1691d28e044e7aa27ab9181ca0502d5ebb182f8a
Albin Hallden <albinhallden@gmail.com>
2016-12-02 18:11:26 +0100

    Rafactor day 02 - first task

--- 02/first.js
+++ 02/first.js
@@ -3,33 +3,27 @@
 const fs = require('fs');
 const path = require('path');
 
-let current = 5;
+let position = { x: 1, y: 1 };
 
 const result = fs.readFileSync(path.join('input.txt')).toString()
   .split('\n')
   .map(item => item.trim())
   .map(item => item.split(''))
-  .map(item => {
-    item.forEach(char => {
-      switch(char) {
-        case 'U':
-          current = current - 3 < 1 ? current : current - 3; 
-          // console.log('U', current);
-          break;
-        case 'D':
-          current = current + 3 > 9 ? current : current + 3;
-          // console.log('D', current);
-          break;
-        case 'L':
-          current = current % 3 === 1 ? current : current - 1;
-          // console.log('L', current);
-          break;
-        case 'R':
-          current = current % 3 === 0 ? current : current + 1;
-          // console.log('R', current);
-          break;
-      }
-    });
-    return current;
-  });
+  .map(item => item.map(char => {
+    if (char === 'U') return { x: 0, y: -1 };
+    if (char === 'D') return { x: 0, y: 1 };
+    if (char === 'L') return { x: -1, y: 0 };
+    if (char === 'R') return { x: 1, y: 0 };
+  }))
+  .map(item => item.reduce((prev, curr) => {
+    let x = curr.x + prev.x;
+    let y = curr.y + prev.y; 
+    x = x >= 0 ? x : 0;
+    y = y >= 0 ? y : 0;
+    x = x <= 2 ? x : 2;
+    y = y <= 2 ? y : 2;
+    position = { x, y };
+    return position;
+  }, position))
+  .map(item => item.y * 3 + item.x % 3 + 1);
 console.log(result);
