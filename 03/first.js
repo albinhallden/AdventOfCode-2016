731977e344c1d398e2d400988340ac81059b3e6c
Albin Hallden <albinhallden@gmail.com>
2016-12-03 09:02:33 +0100

    Add solution for day 3 - first and second task

---
+++ 03/first.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+let position = { x: 1, y: 1 };
+
+const result = fs.readFileSync(path.join('input.txt')).toString()
+  .split('\n')
+  .map(item => item.trim())
+  .map(item => item.split('  '))
+  .map(item => item.map(number => parseInt(number, 10))
+  .sort((b, a) => a < b))
+  .reduce((prev, curr) => {
+    if (curr[0] + curr[1] > curr[2]) prev++;
+    return prev;
+  }, 0);
+console.log(result);
---
+++ 03/second.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+let position = { x: 1, y: 1 };
+
+const result = fs.readFileSync(path.join('input.txt')).toString()
+  .split('\n')
+  .map(item => item.trim())
+  .map(item => item.split(/\s{2,}/))
+  .reduce((prev, curr) => {
+    prev[0].push(curr[0]);
+    prev[1].push(curr[1]);
+    prev[2].push(curr[2]);
+    return prev;
+  }, [[],[],[]])
+  .reduce((prev, curr) => prev.concat(curr), [])
+  .map(item => parseInt(item, 10))
+  .reduce((prev, curr, index, array) => {
+    if ((index + 1) % 3 === 0) prev.push([array[index - 2],array[index - 1],array[index]])
+    return prev;
+  }, [])
+  .map(item => item.sort((a, b) => b < a))
+  .reduce((prev, curr) => curr[0] + curr[1] > curr[2] ?prev + 1 : prev, 0);
+console.log(result);
+
+// 1805 too low
