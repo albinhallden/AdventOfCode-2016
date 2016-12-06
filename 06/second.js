d47744f58c879746b9067dbaf6c206ad8258edd3
Albin Hallden <albinhallden@gmail.com>
2016-12-06 21:15:17 +0100

    Add solutions for day 06

---
+++ 06/second.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const result = fs.readFileSync(path.join('input.txt')).toString()
+  .split('\n').map(item => item.trim())
+  .reduce((prev, curr) => {
+    if (prev.length === 0) prev = new Array(curr.length).fill('');
+    return curr.split('').map((item, index) => prev[index] += item);
+  }, [])
+  .map(item => item.split('').reduce((prev, curr) => {
+    if (prev[curr] === undefined) prev[curr] = 0;
+    prev[curr]++;
+    return prev;
+  }, {}))
+  .map(item => {
+    return Object.keys(item).map(sub => {
+      return { key: sub, count: item[sub] };
+    });
+  })
+  .map(item => item.sort((a, b) => {
+    if (a.count === b.count) return 0;
+    if (a.count > b.count) return 1;
+    return -1;
+  })[0].key).join('');
+console.log(result);
