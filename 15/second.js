5e33b64451ebbe4a0be6549465886b0ee7921c67
Albin Hallden <albinhallden@gmail.com>
2016-12-18 09:13:19 +0100

    Add solutions for day 15

---
+++ 15/second.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const data = fs.readFileSync(path.join('input.txt')).toString()
+  .split('\n').map(item => item.trim())
+  .map(item => item.split(' '))
+  .map((item, index) => {
+    return {
+      length: parseInt(item[3], 10),
+      position: parseInt(item[11], 10)
+    }
+  });
+
+data.push({
+  length: 11,
+  position: 0
+});
+
+let time = 0;
+do {
+  const test = data.map((item, index) => {
+    return (item.position + time + index + 2) % item.length;
+  }).reduce((prev, curr) => prev + curr, 0);
+
+  if (test === 0) break;
+  time++;
+} while (true)
+
+console.log(time + 1);
