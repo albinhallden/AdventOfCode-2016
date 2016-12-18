928f7f1143a05e2820b2f9bc390b068f11318edb
Albin Hallden <albinhallden@gmail.com>
2016-12-18 09:15:37 +0100

    Add solutions for day 18

---
+++ 18/second.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const length = 400000;
+const data = fs.readFileSync('input.txt').toString()
+  .split('\n')
+  .map(item => item.split(''));
+const width = data[0].length;
+
+while (data.length < length) {
+  const prevRow = data[data.length - 1];
+  const row = new Array(width).fill('.')
+    .map((item, index) => {
+      const left = index < 1 ? '.' : prevRow[index - 1];
+      const center = prevRow[index];
+      const right = index >= width - 1 ? '.' : prevRow[index + 1];
+
+      if ( (left === '^' && center === '^' && right === '.')
+        || (left === '.' && center === '^' && right === '^')
+        || (left === '^' && center === '.' && right === '.')
+        || (left === '.' && center === '.' && right === '^')) {
+        return '^';
+      } else {
+        return '.';
+      }
+    });
+
+  data.push(row);
+}
+
+const result = data.reduce((prev, curr) => {
+  return prev + curr.join('').replace(/\^/g, '').length;
+}, 0);
+
+console.log(result);
